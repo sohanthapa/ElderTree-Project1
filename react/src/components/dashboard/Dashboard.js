31d2069f8783e94474201ee619857f8b31927add
paupaugoz <paugozum1@gmail.com>
2019-09-27 17:41:52 -0700

    Clean up SignUp

--- react/src/components/dashboard/Dashboard.js
+++ react/src/components/dashboard/Dashboard.js
@@ -19,7 +19,7 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import AddIcon from '@material-ui/icons/Add';
-import { mainListItems, secondaryListItems } from './listItems';
+import { mainListItems } from './listItems';
 import Modal from './AddModal';
 // import UpdateModal from './UpdateModal';
 import EmployeeTable from './EmployeeTable';
@@ -265,7 +265,6 @@ export default function Dashboard() {
         <Divider />
         <List>{mainListItems}</List>
         <Divider />
-        <List>{secondaryListItems}</List>
       </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
--- react/src/components/dashboard/listItems.js
+++ react/src/components/dashboard/listItems.js
@@ -2,19 +2,16 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import AssignmentIcon from '@material-ui/icons/Assignment';
 
+// eslint-disable-next-line import/prefer-default-export
 export const mainListItems = (
-   <div>
-      <ListItem button>
-         <ListItemIcon>
-            <DashboardIcon />
-         </ListItemIcon>
-         <ListItemText primary="Dashboard" />
-      </ListItem>
-   </div>
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItem>
+  </div>
 );
-
-export const secondaryListItems = <div></div>;
